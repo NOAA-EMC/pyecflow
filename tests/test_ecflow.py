4404d703b16bb6220d6b2a0205024242e3cb764a
Rahul Mahajan <aerorahul@users.noreply.github.com>
2025-09-28 01:27:32 -0400

    add test to check if ecflow python api is importable

--- tests/test_ecflow.py
+++ tests/test_ecflow.py
@@ -1,10 +1,21 @@
 import subprocess
 
 
-def test_ecflow_found():
+class TestEcflow:
+    def test_ecflow_client(self):
 
-    try:
-        result = subprocess.run(['which', 'ecflow_client'], capture_output=True, check=True, text=True)
-        assert True, f"ecflow_client = {result.stdout.strip()}"
-    except subprocess.CalledProcessError:
-        assert False, "ecflow_client not found!"
+        try:
+            result = subprocess.run(['which', 'ecflow_client'], capture_output=True, check=True, text=True)
+            assert True
+            print(f"ecflow_client = {result.stdout.strip()}")
+        except subprocess.CalledProcessError:
+            assert False, "ecflow_client not found!"
+
+    def test_ecflow_import(self):
+        try:
+            import ecflow
+            version = ecflow.__version__
+            assert True
+            print(f"ecflow module imported successfully, version = {version}")
+        except ImportError:
+            assert False, "Failed to import ecflow module!"
