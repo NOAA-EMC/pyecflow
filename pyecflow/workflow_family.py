c042b8242406e5dd2a0f54a23ead81562864a850
Anna Smoot <anna.smoot@noaa.gov>
2026-01-16 21:46:16 +0000

    additional pynorm fixes

--- pyecflow/workflow_family.py
+++ pyecflow/workflow_family.py
@@ -8,3 +8,13 @@ class WorkflowFamily(pf.Family):
     def __init__(self, name: str, famAb_tasks: dict = None, **kwargs):
         super().__init__(name)
         task_triggers = {}
+
+        # If famAb_tasks is provided, create tasks from the dictionary
+        if famAb_tasks is not None:
+            for tt in famAb_tasks.keys():
+                task_tc = famAb_tasks[tt]
+                task = WorkflowTask(tt, task_tc)
+                task_triggers[tt] = task
+                self.add(task)
+
+        self.task_triggers = task_triggers
