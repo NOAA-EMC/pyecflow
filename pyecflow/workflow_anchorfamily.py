edb525577e0c562ca88cc5d98b58d2f6d88e14de
Anna Smoot <anna.smoot@noaa.gov>
2026-01-16 19:42:11 +0000

    additional pynorm fixes

--- pyecflow/workflow_anchorfamily.py
+++ pyecflow/workflow_anchorfamily.py
@@ -4,17 +4,10 @@ from .workflow_task import WorkflowTask
 
 
 class WorkflowAnchorFamily(pf.AnchorFamily):
-    # def __init__(self, name: str, famAb_tasks: dict, **kwargs):
-        # super().__init__(name, variables=context['variables'])
+
     def __init__(self, name: str, famAb_tasks: dict = None, **kwargs):
         super().__init__(name)
         task_triggers = {}
-        # for tt in famAb_tasks.keys(): #should a dict., keys are tasks, tt is the key
-        #    task_tc= famAb_tasks[tt] #get the context dict for this task
-            #task_triggers[tt] = WorkflowTask(tt, task_tc)
-        #    task = WorkflowTask(tt, task_tc)
-        #    task_triggers[tt] = task
-        #    self.add(task)
 
         # If famAb_tasks is provided, create tasks from the dictionary
         if famAb_tasks is not None:
