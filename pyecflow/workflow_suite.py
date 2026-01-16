1e6a588ebfe700758f3e010184f416a20825f859
Anna Smoot <anna.smoot@noaa.gov>
2026-01-16 19:38:06 +0000

    additional pynorm fixes

--- pyecflow/workflow_suite.py
+++ pyecflow/workflow_suite.py
@@ -1,6 +1,8 @@
+# imports
 import pyflow as pf
-#.workflow_task import WorkflowTask
+# .workflow_task import WorkflowTask
 from .workflow_task import WorkflowTask
+
 
 class WorkflowSuite(pf.Suite):
     def __init__(self, name: str, *args, **kwargs):
