d7558456bc812f8fe96a3a023d1739e8ba99439d
Anna Smoot <anna.smoot@noaa.gov>
2026-01-16 11:43:50 -0500

    Delete pyecflow/workflow_task_copy.py

--- pyecflow/workflow_task_copy.py
+++
@@ -1,6 +0,0 @@
-import pyflow as pf
-
-
-class WorkflowTask(pf.Task):
-    def __init__(self, name: str, context: dict, **kwargs):
-        super().__init__(name, variables=context['variables'], script=context['script'])
